1a6804c590ec61598ff31d95cce8af4ec7fa1444
Ceyda Saran <cluise61@outlook.com>
2021-02-03 23:48:51 +0300

    Güncel

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using Business.Concrete;
+using DataAcces.Concrete.EntityFramework;
 using DataAcces.Concrete.InMemory;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(40,100))
             {
                 Console.WriteLine(product.ProductName);
             }
