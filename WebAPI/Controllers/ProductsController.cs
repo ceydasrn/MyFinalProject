5fb65427db31f72635e14cfaeefb7477c4bc95a7
Ceyda Saran <cluise61@outlook.com>
2021-02-14 00:00:35 +0300

    13.02.2021

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -44,7 +44,7 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
-
+        
         [HttpPost("add")]
         public IActionResult Add(Product product)
         {
